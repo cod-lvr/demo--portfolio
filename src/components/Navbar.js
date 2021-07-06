e0b3c52906e2e37ad6e874df1fa986afa89b4d60
Cod-lvr <79340509+cod-lvr@users.noreply.github.com>
2021-07-06 09:47:00 +0200

    add navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import styles from './Navbar.module.css';
 import logo from '../media/logo.png';
-import Main from './Main';
 
 const Navbar = props => {
     return (
-        <div>
-        <nav className={styles.navbar}>
-            <div className={styles.logo}>
-                {/* <img src={logo} /> */}
-                <h3>Logo</h3>
+        <nav>
+            <div className="logo">
+                <img src={logo} />
             </div>
-            <div className={styles.links}>
+            <div className="links">
                 <a href="" >Home</a>
                 <a href="" >About</a>
                 <a href="" >Projects</a>
                 <a href="" >Contact</a>
             </div>
         </nav>
-        <Main />
-        </div>
     )
 };
 
