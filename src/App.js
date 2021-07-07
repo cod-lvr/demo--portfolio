941d83f2edd707bc3f8d38a207a506410bff6b30
cod-lvr <aymang29@yahoo.com>
2021-07-07 14:35:16 +0200

    description

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from "./components/Navbar";
+import Main from './components/Main';
 
 function App() {
   return (
     <React.Fragment>
       <Navbar />
+      <Main />
     </React.Fragment>
   );
 }
