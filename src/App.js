c80ea469ffbb6a03887e3c9f1c28336d1b8adc43
cod-lvr <aymang29@yahoo.com>
2021-07-08 19:48:29 +0200

    add about component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from "./components/Navbar";
 import Main from './components/Main';
+import About from './components/About';
 
 function App() {
   return (
     <React.Fragment>
       <Navbar />
       <Main />
+      <About />
     </React.Fragment>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './About.module.css';
+
+const About = props => {
+    return (
+        <div className={styles.about}>
+            <div className={styles["content-box"]}>
+                <h3>About Me</h3>
+                <p>
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+                </p>
+            </div>
+            <div className={styles.box}>
+                <div className={styles.line}>.</div>
+                <p>It has survived not only five centuries It has survived not only  five centuries.</p>
+            </div>
+        </div>
+    )
+};
+
+export default About;
