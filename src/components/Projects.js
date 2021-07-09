6953f59d58d240d907029e015f1b24ca9008197c
cod-lvr <aymang29@yahoo.com>
2021-07-09 15:06:46 +0200

    description

---
+++ src/components/Projects.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './Projects.module.css';
+
+const Projects = props => {
+    return (
+        <div className={styles.projects}>
+            <h4 className={styles.title}>privious projects</h4>
+            <div className={styles.boxs}>
+                <div className={styles.box}></div>
+                <div className={styles.box}></div>
+                <div className={styles.box}></div>
+                <div className={styles.box}></div>
+            </div>
+        </div>
+    )
+};
+
+export default Projects;
