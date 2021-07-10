39ebc1abbc8bde50e2b9b8f08112198d8a85d9bb
cod-lvr <aymang29@yahoo.com>
2021-07-11 00:18:07 +0200

    modified App component for fontawesome

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from "./components/Navbar";
 import Main from './components/Main';
 import About from './components/About';
+import Skills from './components/Skills';
 import Projects from './components/Projects';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Main />
       <About />
+      <Skills />
       <Projects />
     </React.Fragment>
   );
