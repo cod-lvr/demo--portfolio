0ad84aedb45fd1b81e87851fcf4ad7713ad4dd24
cod-lvr <aymang29@yahoo.com>
2021-07-20 19:28:54 +0200

    add first project img to the projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import styles from './Projects.module.css';
+import v1 from '../media/V1.png';
 
 const Projects = props => {
     return (
         <div className={styles.projects} id="projects">
             <h4 className={styles.title}>privious projects</h4>
             <div className={styles.boxs}>
-                <div className={styles.box}></div>
+                <div className={styles.box}>
+                    <img src={v1} alt="photo" />
+                </div>
                 <div className={styles.box}></div>
                 <div className={styles.box}></div>
                 <div className={styles.box}></div>
