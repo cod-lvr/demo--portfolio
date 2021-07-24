60dee17ae16d8302cbf533787c492fd75268355f
cod-lvr <aymang29@yahoo.com>
2021-07-24 22:32:39 +0200

    description

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,16 +7,16 @@ const Skills = props => {
         <div className={styles.skills}>
             <h3 className={styles.title}>skills</h3>
             <div className={styles.icons}>
-                <span className={styles.icon} style={{color: "#61DBFB"}}><i className="fab fa-react fa-5x"></i></span>
+                <span className={styles.icon} style={{color: "#61DBFB"}}><i className="fab fa-react fa-5x"></i><span className={styles.tooltiptext}>React.js</span></span>
                 <span className={styles.icon} style={{color: "#f0db4f"}}><i className="fab fa-js-square fa-5x"></i></span>
-                <span className={styles.icon} style={{color: "#f06529"}}><i className="fab fa-html5 fa-5x"></i></span>
-                <span className={styles.icon} style={{color: "#2965f1"}}><i className="fab fa-css3-alt fa-5x"></i></span>
-                <span className={styles.icon} style={{color: "#211F1F"}}><i className="fab fa-github-square fa-5x"></i></span>
-                <span className={styles.icon} style={{color: "#68A063"}}><i class="fab fa-node fa-5x"></i></span>
+                <span className={styles.icon} style={{color: "#f06529"}}><i className="fab fa-html5 fa-5x"></i><span className={styles.tooltiptext}>html5</span></span>
+                <span className={styles.icon} style={{color: "#2965f1"}}><i className="fab fa-css3-alt fa-5x"></i><span className={styles.tooltiptext}>CSS</span></span>
+                <span className={styles.icon} style={{color: "#211F1F"}}><i className="fab fa-github-square fa-5x"></i><span className={styles.tooltiptext}>Github</span></span>
+                <span className={styles.icon} style={{color: "#68A063"}}><i class="fab fa-node fa-5x"></i><span className={styles.tooltiptext}>node.js</span></span>
             </div>
             <div className={styles.content}>
-                <p>it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting</p>
-                <Button>Download Cv</Button>
+                <p>So far I have JavaScript, HTML, CSS,jQuery, Bootstrap, React.Js,Tailwind and Git/GitHub under my belt. I've started learning Node.JS,MongoDB, and Express.JS. I'm still enthusiastically grabbing onto any other programming languages,frameworks,or principles I can integrate into the coding web in my head</p>
+                <Button>cONTACT ME</Button>
             </div>
         </div>
     )
