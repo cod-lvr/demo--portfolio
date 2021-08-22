f05ce7dae41eb2dce1b817c98718a2f25b3de421
cod-lvr <aymang29@yahoo.com>
2021-08-22 05:48:22 +0200

    modified about component

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import styles from './About.module.css';
+import React from "react";
+import { motion } from "framer-motion";
+import styles from "./About.module.css";
 
-const About = props => {
-    return (
-        <div className={styles.about} id="about" >
-            <div className={styles["content-box"]}>
-                <h3 className={styles["title"]}>About Me</h3>
-                <h2>it started with a dream and vision.</h2>
-            </div>
-            <div className={styles.box}>
-                <p> A self-thought passionate Front-End Developer having an experience of building Web applications with JavaScript / Reactjs / Nodejs and some other cool libraries and frameworks and design patterners.</p>
-            </div>
-        </div>
-    )
+const About = (props) => {
+  return (
+    <motion.div
+      className={styles.about}
+      id="about"
+      initial={{ opacity: 0, x: -100 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 1, delay: 2 }}
+    >
+      <div className={styles["content-box"]}>
+        <h3 className={styles["title"]}>About Me</h3>
+        <h2>it started with a dream and vision.</h2>
+      </div>
+      <div className={styles.box}>
+        <p>
+          A self-thought passionate Front-End Developer having an experience of
+          building Web applications with JavaScript / Reactjs / Nodejs and some
+          other cool libraries and frameworks and design patterners.
+        </p>
+      </div>
+    </motion.div>
+  );
 };
 
 export default About;
