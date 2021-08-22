e31751b9ce9b477720c5f4e3a7ec4a73d4509c9a
cod-lvr <aymang29@yahoo.com>
2021-08-22 05:52:25 +0200

    add framer-motion animation liberary

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,14 +1,24 @@
-import styles from './Card.module.css';
+import { motion } from "framer-motion";
+import styles from "./Card.module.css";
 
-const Card = props => {
-    return (
-        <div className={styles["card"]}>
-            <span className={styles["logo"]}>{props.logo}</span>
-                <h3 className={styles["title"]}>{props.title}</h3>
-                <p>{props.content}</p>
-                <button className={styles["arrow-btn"]}><a href={props.href} target="_blank"><i class="fas fa-chevron-down"></i></a></button>
-        </div>
-    )
+const Card = (props) => {
+  return (
+    <motion.div
+      className={styles["card"]}
+      animate={{  boxShadow: "0px 0px 8px rgb(0, 0, 0)" }}
+      transition={{duration: .14}}
+      whileHover={{boxShadow: "none" }}
+    >
+      <span className={styles["logo"]}>{props.logo}</span>
+      <h3 className={styles["title"]}>{props.title}</h3>
+      <p>{props.content}</p>
+      <button className={styles["arrow-btn"]}>
+        <a href={props.href} target="_blank">
+          <i class="fas fa-chevron-down"></i>
+        </a>
+      </button>
+    </motion.div>
+  );
 };
 
 export default Card;
